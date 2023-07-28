989495585f8da3831b17b1a779b7eef0181c533e
Pawan_Maurya <mauryapawan010011@gmail.com>
2023-07-28 12:31:15 +0530

    implemented socket url

--- socket/index.js
+++ socket/index.js
@@ -1,4 +1,4 @@
-const io = require("socket.io")({
+const io = require("socket.io")("https://namaskaram-socketio.vercel.app", {
     cors: {
         origin: "https://namaskaram-client.vercel.app",
     },
