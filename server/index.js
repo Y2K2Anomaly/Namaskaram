364ea14ead74546cec4feed3f7dc54cf18365714
Pawan_Maurya <mauryapawan010011@gmail.com>
2023-07-29 19:47:09 +0530

    setup links for vercel

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(morgan("common"));
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "https://namaskaram-client.vercel.app"
 }));
 
 // routes import
