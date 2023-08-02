ffe71bad967e7333b1d9c3b861bbaad5a45f2d82
Pawan_Maurya <mauryapawan010011@gmail.com>
2023-08-02 19:17:21 +0530

    fixing bug related to socket.io

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -76,7 +76,9 @@ const Rightbar = React.memo(() => {
     // Fetching Online Friends
     useEffect(() => {
         const fetchOnlineFriends = async () => {
-            const socket = io("https://namaskaram-socketio.vercel.app");
+            const socket = io("https://namaskaram-socketio.vercel.app", {
+                transports: ["websocket"], // websocket transport to avoid CORS issues after app deployment
+            });
 
             // Emitting 'addUser' event to let the server know about the current user
             socket.emit("addUser", currentUser._id);
