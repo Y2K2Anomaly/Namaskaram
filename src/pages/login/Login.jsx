5a8509b7d13b848bd70b2d91cc64eae5e480a5ca
Pawan_Maurya <mauryapawan010011@gmail.com>
2023-08-21 22:25:50 +0530

    added progressbar while register and login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,13 +2,13 @@ import { useContext, useRef } from "react";
 import "./login.css";
 import { loginCall } from "../../apiCalls";
 import { AuthContext } from "../../context/AuthContext";
-import { CircularProgress } from "@mui/material";
+import { CircularProgress, LinearProgress } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
   const email = useRef();
   const password = useRef();
-  const { isFetching, dispatch } = useContext(AuthContext);
+  const { dispatch, isFetching } = useContext(AuthContext);
   const navigate = useNavigate();
 
   // Login as a test user
@@ -28,52 +28,57 @@ export default function Login() {
   };
 
   return (
-    <div className="login">
-      <div className="loginWrapper">
-        <div className="loginLeft">
-          <div className="namaskaramTitle">
-            <h3 className="loginLogo">
-              Namaskaram
-            </h3>
-            <img src='/assets/namaste_icon.png' alt='#logo' className='loginNamasteIcon' />
-          </div>
+    <>
+      {isFetching &&
+        <LinearProgress />
+      }
+      <div className="login">
+        <div className="loginWrapper">
+          <div className="loginLeft">
+            <div className="namaskaramTitle">
+              <h3 className="loginLogo">
+                Namaskaram
+              </h3>
+              <img src='/assets/namaste_icon.png' alt='#logo' className='loginNamasteIcon' />
+            </div>
 
-          <span className="loginDesc">
-            Connect with friends and the world around you on Namaskaram.
-          </span>
-        </div>
-        <div className="loginRight">
-          <form className="loginBox" onSubmit={handleClick}>
-            <h1>Already Registered?</h1>
-            <Link onClick={() => getTestUser()} className="testUserLogin">Login as a test user!</Link>
-            <input
-              placeholder="Email"
-              type="email"
-              required
-              className="loginInput"
-              ref={email}
-            />
-            <input
-              placeholder="Password"
-              type="password"
-              required
-              minLength="6"
-              className="loginInput"
-              ref={password}
-            />
-            <button className="loginButton" type="submit" disabled={isFetching}>
-              {isFetching ? (
-                <CircularProgress color="inherit" size="20px" />
-              ) : (
-                "Log In"
-              )}
-            </button>
-            <button className="loginRegisterButton" onClick={() => navigate('/register')}>
-              Create a New Account
-            </button>
-          </form>
+            <span className="loginDesc">
+              Connect with friends and the world around you on Namaskaram.
+            </span>
+          </div>
+          <div className="loginRight">
+            <form className="loginBox" onSubmit={handleClick}>
+              <h1>Already Registered?</h1>
+              <Link onClick={() => getTestUser()} className="testUserLogin">Login as a test user!</Link>
+              <input
+                placeholder="Email"
+                type="email"
+                required
+                className="loginInput"
+                ref={email}
+              />
+              <input
+                placeholder="Password"
+                type="password"
+                required
+                minLength="6"
+                className="loginInput"
+                ref={password}
+              />
+              <button className="loginButton" type="submit" disabled={isFetching}>
+                {isFetching ? (
+                  <CircularProgress color="inherit" size="20px" />
+                ) : (
+                  "Log In"
+                )}
+              </button>
+              <button className="loginRegisterButton" onClick={() => navigate('/register')}>
+                Create a New Account
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useRef, useState } from 'react';
 import "./register.css";
 import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
-import { IconButton } from "@mui/material";
+import { LinearProgress, IconButton } from "@mui/material";
 import { AddAPhoto } from "@mui/icons-material";
 import { loginCall } from '../../apiCalls';
 import { AuthContext } from '../../context/AuthContext';
@@ -23,7 +23,7 @@ const Register = () => {
 
 
     // Login as a test_user
-    const { dispatch } = useContext(AuthContext);
+    const { dispatch, isFetching } = useContext(AuthContext);
 
     const getTestUser = (async () => {
         loginCall(
@@ -95,132 +95,139 @@ const Register = () => {
     };
 
     return (
-        <div className="register">
-            <div className="registerWrapper">
-                <div className="registerLeft">
-                    <div className="namaskaramTitle">
-                        <h3 className="loginLogo">
-                            Namaskaram
-                        </h3>
-                        <img src='/assets/namaste_icon.png' alt='#logo' className='loginNamasteIcon' />
-                    </div>
-                    <span className='registerDesc'>Connect with friends and the world around you on Namaskaram.</span>
-                </div>
-                <div className="registerRight">
-                    <form className="registerBox" onSubmit={handleClick}>
-                        <h1>Register Account</h1>
-                        <Link onClick={() => getTestUser()} className='testUserLogin'>Login as a test user!</Link>
-                        <div className='userImage'>
-                            <img src={file ? URL.createObjectURL(file) : "assets/noAvatar.png"} alt="" />
-                            <div className="addRegisterImageButton">
-                                <IconButton>
-                                    <AddAPhoto color="primary" sx={{ fontSize: 28 }} />
-                                    <input
-                                        name="file"
-                                        type="file"
-                                        id="file"
-                                        accept=".png, .jpeg, .jpg"
-                                        onChange={(e) => setFile(e.target.files[0])}
-                                    />
-                                </IconButton>
-                            </div>
+        <>
+            {isFetching &&
+                <LinearProgress />
+            }
+            <div className="register">
+                <div className="registerWrapper">
+                    <div className="registerLeft">
+                        <div className="namaskaramTitle">
+                            <h3 className="loginLogo">
+                                Namaskaram
+                            </h3>
+                            <img src='/assets/namaste_icon.png' alt='#logo' className='loginNamasteIcon' />
                         </div>
-                        <input
-                            type="text"
-                            className="registerInput"
-                            placeholder="Name"
-                            ref={name}
-                            required
-                        />
-                        <input
-                            type="text"
-                            className="registerInput"
-                            placeholder="@username"
-                            value={username}
-                            onChange={handleUsernameChange}
-                            required
-                        />
-                        {!isValid && (
-                            <div style={{ color: 'red' }}>
-                                Username must contain only small letters, numbers, or the special characters like ' @ ', ' _ ' and should starts with ' @ '. It does not start with ' _ ' or have ' _ ' after the ' @ '. (no spaces allowed)
+                        <span className='registerDesc'>Connect with friends and the world around you on Namaskaram.</span>
+                    </div>
+                    <div className="registerRight">
+                        <form className="registerBox" onSubmit={handleClick}>
+                            <h1>Register Account</h1>
+                            <Link onClick={() => getTestUser()} className='testUserLogin'
+                            >Login as a test user!
+                            </Link>
+                            <div className='userImage'>
+                                <img src={file ? URL.createObjectURL(file) : "assets/noAvatar.png"} alt="" />
+                                <div className="addRegisterImageButton">
+                                    <IconButton>
+                                        <AddAPhoto color="primary" sx={{ fontSize: 28 }} />
+                                        <input
+                                            name="file"
+                                            type="file"
+                                            id="file"
+                                            accept=".png, .jpeg, .jpg"
+                                            onChange={(e) => setFile(e.target.files[0])}
+                                        />
+                                    </IconButton>
+                                </div>
                             </div>
-                        )}
-                        <input
-                            type="text"
-                            className="registerInput"
-                            placeholder="Bio"
-                            ref={bio}
-                            required
-                        />
-                        <input
-                            type="text"
-                            className="registerInput"
-                            placeholder="City"
-                            ref={city}
-                            required
-                        />
-                        <input
-                            type="text"
-                            className="registerInput"
-                            placeholder="From"
-                            ref={from}
-                            required
-                        />
-                        <strong>DOB:</strong>
-                        <input
-                            type="date"
-                            className="registerInput"
-                            placeholder="Date of Birth"
-                            ref={dateOfBirth}
-                            required
-                        />
-                        <input
-                            type="text"
-                            className="registerInput"
-                            placeholder="Relationship eg. 1 - 2 - 3"
-                            ref={relationship}
-                            required
-                        />
-                        <input
-                            type="email"
-                            className="registerInput"
-                            placeholder="Email"
-                            ref={email}
-                            required
-                        />
-                        <input
-                            type="password"
-                            className="registerInput"
-                            placeholder="Password"
-                            ref={password}
-                            minLength="6"
-                            required
-                        />
-                        <input
-                            type="password"
-                            className="registerInput"
-                            placeholder="Password Again"
-                            ref={passwordAgain}
-                            required
-                        />
-                        <button
-                            className="registerButton"
-                            type='submit'
-                            onClick={() => navigate('/register')}
-                        >
-                            Sign Up
-                        </button>
-                        <button
-                            className="registerLoginButton"
-                            onClick={() => navigate('/login')}
-                        >
-                            Log into Account
-                        </button>
-                    </form>
-                </div>
+                            <input
+                                type="text"
+                                className="registerInput"
+                                placeholder="Name"
+                                ref={name}
+                                required
+                            />
+                            <input
+                                type="text"
+                                className="registerInput"
+                                placeholder="@username"
+                                value={username}
+                                onChange={handleUsernameChange}
+                                required
+                            />
+                            {!isValid && (
+                                <div style={{ color: 'red' }}>
+                                    Username must contain only small letters, numbers, or the special characters like ' @ ', ' _ ' and should starts with ' @ '. It does not start with ' _ ' or have ' _ ' after the ' @ '. (no spaces allowed)
+                                </div>
+                            )}
+                            <input
+                                type="text"
+                                className="registerInput"
+                                placeholder="Bio"
+                                ref={bio}
+                                required
+                            />
+                            <input
+                                type="text"
+                                className="registerInput"
+                                placeholder="City"
+                                ref={city}
+                                required
+                            />
+                            <input
+                                type="text"
+                                className="registerInput"
+                                placeholder="From"
+                                ref={from}
+                                required
+                            />
+                            <strong>DOB:</strong>
+                            <input
+                                type="date"
+                                className="registerInput"
+                                placeholder="Date of Birth"
+                                ref={dateOfBirth}
+                                required
+                            />
+                            <input
+                                type="text"
+                                className="registerInput"
+                                placeholder="Relationship eg. 1 - 2 - 3"
+                                ref={relationship}
+                                required
+                            />
+                            <input
+                                type="email"
+                                className="registerInput"
+                                placeholder="Email"
+                                ref={email}
+                                required
+                            />
+                            <input
+                                type="password"
+                                className="registerInput"
+                                placeholder="Password"
+                                ref={password}
+                                minLength="6"
+                                required
+                            />
+                            <input
+                                type="password"
+                                className="registerInput"
+                                placeholder="Password Again"
+                                ref={passwordAgain}
+                                required
+                            />
+                            <button
+                                className="registerButton"
+                                type='submit'
+                                onClick={() => navigate('/register')}
+                            >
+                                Sign Up
+                            </button>
+                            <button
+                                className="registerLoginButton"
+                                onClick={() => navigate('/login')}
+                            >
+                                Log into Account
+                            </button>
+                        </form>
+                    </div>
 
-            </div>
-        </div >
+                </div>
+            </div >
+        </>
     )
 }
 
